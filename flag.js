084956e8d5ecd08d2ed0edc266b5d95fc69bcc14
emceelamb <mlam@berkeley.edu>
2017-07-24 20:53:06 -0700

    refactored code for a live type-thru

--- flag.js
+++ flag.js
@@ -1,3 +1,93 @@
+/// USA Flag
+
+
+// Type thru code
+
+x=0;
+y=0;
+while(x<14){
+  sleep(1000);
+  x++;
+  if(x<9){
+  if(x%2!=0){
+    redStar();
+  } else {
+    whiteStar();
+  }
+  y=0;
+  }
+  if(x>9){
+    if(x%2==0){
+      redStripe();
+    } else {
+      whiteStripe();
+    }
+  }
+}
+
+
+//// Refactored functions
+
+function redStripe(){
+
+    while(y<59){
+      y++;
+      process.stdout.write(c.bold.red('='))
+    }
+    process.stdout.write('\n');
+    y=0;
+
+}
+
+function whiteStripe(){
+  while(y<59){
+    y++;
+    process.stdout.write(c.bold.white('='))
+  }
+  process.stdout.write('\n');
+  y=0;
+
+}
+
+function whiteStar(){
+  while(y<60){
+    y++;
+    if(y<22){
+      if(y%2!=0){
+        process.stdout.write(c.bold.white('*'));
+      } else {
+        process.stdout.write(c.bold.blue('='));
+      }
+    }
+    if(y>22) {
+        process.stdout.write(c.bold.white('='));
+      }
+  }
+  process.stdout.write('\n')
+}
+
+function redStar(){
+  while(y<60){
+    y++;
+    if(y<22){
+      if(y%2!=0){
+        process.stdout.write(c.blue.bold('='));
+      } else {
+        process.stdout.write(c.white.bold('*'));
+      }
+    }
+    if(y>22) {
+        process.stdout.write(c.bold.red('='));
+      }
+  }
+  process.stdout.write('\n');
+  y=0;
+}
+
+
+
+
+//// full code
 x=0;
 y=0;
 while(x<14){
--- terminaldrawing.js
+++ terminaldrawing.js
@@ -48,4 +48,6 @@ function sleep(milliseconds) {
 //
 //
 // // welcome();
+
+
 clear();
