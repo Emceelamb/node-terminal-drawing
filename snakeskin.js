549b70552597fe5446ec5812b683f4a3701004d7
emceelamb <mlam@berkeley.edu>
2017-07-30 22:03:47 -0700

    added color to snakeskin

--- snakeskin.js
+++ snakeskin.js
@@ -1,27 +1,61 @@
 // Snake Skin Pattern
 // Made in a live coding session
+function snakeskin(){
+	for(var x=0; x<20;x++){
+	    tree();
+	    process.stdout.write('\n');
+	    sleep(200);
+	    v();
+	    process.stdout.write('\n');
+	    sleep(200);
+	    if(x%6==0){
+				treer();
+				vr();
+	        sleep(200);
+	    }
+	    if(x%8==0){
+				treey();
+				vy();
+	      sleep(200);
+	    }
 
-for(var x=0; x<20;x++){
-    tree();
-    process.stdout.write('\n');
-    sleep(400);
-    v();
-    process.stdout.write('\n');
-    sleep(400);
+	}
 }
+
 
 function left(){
   process.stdout.write(c.green('\\'));
 }
 
 function right(){
-  process.stdout.write(c.green('/ '));
+  process.stdout.write(c.green('/'));
 }
+
+
+function leftred(){
+  process.stdout.write(c.red('\\'));
+}
+
+function rightred(){
+  process.stdout.write(c.red('/'));
+}
+
+
+function leftyel(){
+  process.stdout.write(c.yellow('\\'));
+}
+
+function rightyel(){
+  process.stdout.write(c.yellow('/'));
+}
+
+
 
 function tree(){
   for(var i = 0 ; i <20; i++){
     right();
     left();
+
   }
 }
 
@@ -30,4 +64,40 @@ function v (){
     left();
     right();
   }
+}
+
+function treer(){
+  for(var i = 0 ; i <20; i++){
+    rightred();
+    leftred();
+
+  }
+	process.stdout.write('\n');
+}
+
+function vr (){
+  for(var i=0;i<20;i++){
+    leftred();
+    rightred();
+
+  }
+	process.stdout.write('\n');
+}
+
+function treey(){
+  for(var i = 0 ; i <20; i++){
+    rightyel();
+    leftyel();
+  }
+	process.stdout.write('\n');
+
+}
+
+function vy (){
+  for(var i=0;i<20;i++){
+    leftyel();
+    rightyel();
+
+  }
+	process.stdout.write('\n');
 }
